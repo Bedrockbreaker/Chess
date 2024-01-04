00e4265c6fe0b2832f28c45a9648710a0ce74689
Bedrockbreaker <coolmandude101@gmail.com>
2024-01-03 17:27:01 -0700

    Prep for React+Vite

---
+++ src/frontend/components/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "../assets/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><div>Hello World!</div></React.StrictMode>);
